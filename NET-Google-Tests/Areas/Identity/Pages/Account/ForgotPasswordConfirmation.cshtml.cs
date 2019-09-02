fc6cee019150f4e14d8ef18e236cbda931f06080
deliotclarke <deliotclarke@gmail.com>
2019-09-02 10:42:05 -0500

    spun up identity for some practice, wanted to override some pages I haven't had a chance to look at yet.

---
+++ NET-Google-Tests/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NET_Google_Tests.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
