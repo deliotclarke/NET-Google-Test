db7a9a502930827ce157465fa58adf3fa3e8e23f
deliotclarke <deliotclarke@gmail.com>
2019-09-03 09:51:41 -0500

    added a class, added an audio file of myself speaking

---
+++ NET-Google-Tests/Models/FirstTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NET_Google_Tests.Models
+{
+    public class FirstTest
+    {
+
+    }
+}
